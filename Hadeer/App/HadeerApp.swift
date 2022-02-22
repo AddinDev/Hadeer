b6a32b183a8de0a3c7a3ae8dbba025447e7ce9cc
AddinDev <addinsatria2004@gmail.com>
2022-02-22 19:50:40 +0700

    Initial Commit

---
+++ Hadeer/App/HadeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadeerApp.swift
+//  Hadeer
+//
+//  Created by Addin Satria on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HadeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
